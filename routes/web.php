adc27a246fb3e041dfa2235f8df2e4ebff55a123
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-16 18:43:13 +0900

    ルーティングの修正

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ホーム
+Route::get('/', 'HomeController@index')->name('home');
+
+// 管理者ログイン
+Route::post('login', 'LoginController@login')->name('login');
+// ログアウト
+Route::post('logout', 'LoginController@logout')->name('logout');
+
+// 項目の作成
+Route::post('/', 'GagController@create');
+// 項目の詳細
+Route::get('/{id}', 'GagController@show');
+// 項目の更新
+Route::post('/{id}', 'GagController@update');
+// 項目の削除
+Route::post('/{id}/delete', 'GagController@delete');
+
+// ログの閲覧
+Route::get('log', 'LogController@index')->name('log');
