18a2f71f468ab80fb0ea76c4b22a8ed549e4bf75
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-17 18:21:09 +0900

    ログ一覧を表示する

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Gag;
 
 class LogController extends Controller
 {
     public function index() 
     {
-
+        $gags = Gag::all();
+        return view('log', ['gags' => $gags]);
     }
 }
