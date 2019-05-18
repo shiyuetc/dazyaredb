2c2e4a6dfc34f6d6dc04c0aa1a0abc5c232da7e1
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-18 15:42:37 +0900

    タイムスタンプの自動更新を無効化

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -4,15 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Gag extends Model
+class Log extends Model
 {
     public $timestamps = false;
-    
-    public $incrementing = false;
-    
-    protected $keyType = 'string';
 
     protected $fillable = [
-        'id', 'text', 'yomi',
+        'user_id', 'name', 'gag_id', 'command', 'before_text', 'before_yomi', 'after_text', 'after_yomi', 'created_at'
     ];
 }
