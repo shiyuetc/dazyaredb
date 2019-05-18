53bba8c790a72316400b836c5e8605a665ba28a9
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-18 09:24:24 +0900

    指定IDのだじゃれが無いとエラーページにリダイレクトするように修正

--- app/Http/Controllers/GagController.php
+++ app/Http/Controllers/GagController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Gag;
 
 class GagController extends Controller
 {
     public function show($id) 
     {
-
+        $gag = Gag::find($id);
+        if(is_null($gag)) 
+        {
+            return view('errors.404');
+        }
+        else 
+        {
+            return view('gag', ['gag' => $gag]);
+        }
     }
 
     public function create() 
