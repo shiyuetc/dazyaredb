9ff15117aa3f48340a726ac6cbe368d4c828fc36
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-19 14:57:55 +0900

    Logの取得方法を変更

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Gag;
+use App\Models\Log;
 
 class LogController extends Controller
 {
     public function index() 
     {
-        $gags = Gag::all();
-        return view('log', ['gags' => $gags]);
+        return view('pages.log', ['logs' => Log::getPage(1)]);
     }
 }
--- app/Models/Log.php
+++ app/Models/Log.php
@@ -11,4 +11,14 @@ class Log extends Model
     protected $fillable = [
         'user_id', 'name', 'gag_id', 'command', 'before_text', 'before_yomi', 'after_text', 'after_yomi', 'created_at'
     ];
+
+    private static $getCount = 200;
+
+    public static function getPage($page = 1)
+    {
+        return Log::orderBy('id', 'desc')
+            ->offset(($page - 1) * self::$getCount)
+            ->limit(self::$getCount)
+            ->get();
+    }
 }
