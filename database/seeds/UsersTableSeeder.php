2493b087e83d4a490932f39096c424ad123d497b
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-20 10:10:50 +0900

    fix generate method for remember_token

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,19 +12,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            [ 'id' => 'test1', 'name' => 'test_user1', 'password' => bcrypt('test1'), 'remember_token' => self::createRememberToken() ],
-            [ 'id' => 'test2', 'name' => 'test_user2', 'password' => bcrypt('test2'), 'remember_token' => self::createRememberToken() ],
-            [ 'id' => 'test3', 'name' => 'test_user3', 'password' => bcrypt('test3'), 'remember_token' => self::createRememberToken() ],
+            [ 'id' => 'test1', 'name' => 'test_user1', 'password' => bcrypt('test1'), 'remember_token' => str_random(10) ],
+            [ 'id' => 'test2', 'name' => 'test_user2', 'password' => bcrypt('test2'), 'remember_token' => str_random(10) ],
+            [ 'id' => 'test3', 'name' => 'test_user3', 'password' => bcrypt('test3'), 'remember_token' => str_random(10) ],
         ]);
-    }
-
-    public static function createRememberToken()
-    {
-        $str = array_merge(range('a', 'z'), range('0', '9'), range('A', 'Z'));
-        $r_str = null;
-        for ($i = 0; $i < 60; $i++) {
-            $r_str .= $str[rand(0, count($str) - 1)];
-        }
-        return $r_str;
     }
 }
